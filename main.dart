f6e5ddafb1131ebfd48413dc2d702479e8378df0
Saqlainalyshah <saqlainalishah.official@gmail.com>
2025-02-18 13:56:59 +0500

    quick code

--- main.dart
+++ main.dart
@@ -1,2 +1,47 @@
+/// useful quick code to save time;
 
-dart program use
+///copywith Constructor to store value in a model class and get it anywhere in flutte project;
+class TripDataClass {
+  String destination;
+  DateTime checkIn;
+  DateTime checkOut;
+  String partner;
+  List<String> interestList;
+  String interestListOptional;
+  String budgetType;
+  String specificBudgetTypeOptional;
+
+  TripDataClass({
+    required this.destination,
+    required this.checkIn,
+    required this.checkOut,
+    required this.partner,
+    required this.interestList,
+    required this.interestListOptional,
+    required this.budgetType,
+    required this.specificBudgetTypeOptional,
+  });
+
+  TripDataClass copyWith({
+    String? destination,
+    DateTime? checkIn,
+    DateTime? checkOut,
+    String? partner,
+    List<String>? interestList,
+    String? interestListOptional,
+    String? budgetType,
+    String? specificBudgetTypeOptional,
+  }) {
+    return TripDataClass(
+      destination: destination ?? this.destination,
+      checkIn: checkIn ?? this.checkIn,
+      checkOut: checkOut ?? this.checkOut,
+      partner: partner ?? this.partner,
+      interestList: interestList ?? this.interestList,
+      interestListOptional: interestListOptional ?? this.interestListOptional,
+      budgetType: budgetType ?? this.budgetType,
+      specificBudgetTypeOptional:
+          specificBudgetTypeOptional ?? this.specificBudgetTypeOptional,
+    );
+  }
+}
